c177a8baf3531309fed48e001ae56f43b39a2d7f
dugideek <daniel.keener86@gmail.com>
2018-04-18 15:54:45 -0400

    Initial commit

---
+++ subscription/src/app/fancyText/fancyText.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fancy-text',
+  template: `
+<input type='text'/>
+  `,
+  styleUrls: ["./fancyText.component.css"]
+})
+export class FancyText {
+
+}
---
+++ subscription/src/app/subscription-form/subscription-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'subscription-form',
+  templateUrl: "./subscription-form.component.html",
+  styles: []
+})
+export class SubscriptionFormComponent implements OnInit {
+
+  @Input() interests: [string]
+
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
